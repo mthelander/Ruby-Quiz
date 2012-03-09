b51537af68458bb674ca3504a91b45f6c0689f38
Michael Thelander <michael.thelander@rentrakmail.com>
2012-03-09 11:01:47 -0800

    Modify Regexp builder to work with large numbers
    Unfinished

--- regexp_build/regexp_build.rb
+++ regexp_build/regexp_build.rb
@@ -1,10 +1,54 @@
-module RubyQuiz
-  def Regexp.build(*args)
-    a = args.map do |n|
-      n.is_a?(Range) ? n.to_a : n
-    end.flatten.partition do |n|
-      n < 0
-    end.map(&:sort).map(&:reverse).flatten
-    /(#{a*?|})/
+class Regexp
+  def self.build(*args)
+    a = args.group_by(&:class)
+    integers, ranges = a[Fixnum] || [], a[Range] || []
+    Regexp.union(*ranges.map(&:to_r), *integers.sort.reverse.map(&:to_r))
+  end
+end
+
+class Range
+  def to_r
+    max_length = self.last.to_s.size
+    first, last = self.first, self.last
+    left, right = [ first.round + 10, first ].min, [ last.round, last ].max
+
+    #p "first: #{first}, last: #{last}, left: #{left}, right: #{right}"
+    #p "#{first..left-1}, #{left..right}, #{right+1..last}"
+    Regexp.union([first..left-1, left..right, right+1..last].map(&:_to_r).compact)
+  end
+
+  def _to_r
+    return nil if self.to_a.empty?
+    max_length, $; = self.last.to_s.size, ''
+    a, b = [ self.first, self.last ].map do |n|
+      n.to_s.rjust(max_length, ?0).split.map(&:to_i)
+    end
+
+    Regexp.compile("^#{a.zip(b).map(&:to_r).join}$")
+  end
+end
+
+class Array
+  def to_r
+    case
+      when self.first == self.last
+        self.first.to_r(false)
+      #when (0..9) === self.first && (0..9) === self.last
+      #  "^[#{self.first}-#{self.last}]$"
+      else
+        "[#{[self.first, self.last]*?-}]"
+    end
+  end
+end
+
+class Fixnum
+  def round
+    self - (self % 10)
+  end
+
+  def to_r(wrap=true)
+    l, r = wrap ? ['^[', ']$' ] : [ ?[, ?] ]
+    l.insert(1, ?-) if self < 0
+    l + self.abs.to_s + r
   end
 end
--- regexp_build/regexp_build_spec.rb
+++ regexp_build/regexp_build_spec.rb
@@ -1,5 +1,4 @@
 require '~/work/rubyquiz/regexp_build/regexp_build.rb'
-include RubyQuiz
 
 describe "Regexp" do
   before :each do
@@ -7,21 +6,71 @@ describe "Regexp" do
     @deny   = ->(r, a) { a.none? { |s| r.match(s) }.should be_true }
   end
 
-  it "should be able to handle a mix of integers and ranges" do
-    r = Regexp.build(1, 2, 3, 4..6, 7)
-    @assert[r, %w[ 1 2 3 4 5 6 7 ]]
-    @deny[r, %w[ 0 8 9 ]]
-  end
-
-  it "should handle double digit characters" do
-    r = Regexp.build(10..15)
-    @assert[r, %w[ 10 11 12 13 14 15 ]]
-    @deny[r, %w[ 1 2 3 4 5 ]]
-  end
+#  it "should be able to handle a mix of integers and ranges" do
+#    r = Regexp.build(1, 2, 3, 4..6, 7)
+#    @assert[r, %w[ 1 2 3 4 5 6 7 ]]
+#    @deny[r, %w[ 0 8 9 ]]
+#  end
+#
+#  it "should handle double digit characters" do
+#    r = Regexp.build(10..15)
+#    @assert[r, %w[ 10 11 12 13 14 15 ]]
+#    @deny[r, %w[ 1 2 3 4 5 ]]
+#  end
 
   it "should handle negative numbers" do
     r = Regexp.build(-3, -4, -16)
+    p sanitize r
     @assert[r, %w[ -3 -4 -16 ]]
     @deny[r, %w[3 4 16 6 1 ]]
   end
+
+#  it "should handle a large number" do
+#    r = Regexp.build(1..1_000_000)
+#    p sanitize r
+#    @assert[r, (1..1_000_000).map(&:to_s)]
+#    @deny[r, %w[ 1000001 ]]
+#  end
+#
+#  it "should not die" do
+#    lucky = Regexp.build( 3, 7 )
+#    p lucky
+#    ("7"    =~ lucky).should be_true
+#    ("13"   =~ lucky).should be_false
+#    ("3"    =~ lucky).should be_true
+#
+#    month = Regexp.build( 1..12 )
+#    ("0"    =~ month).should be_false
+#    ("1"    =~ month).should be_true
+#    ("12"   =~ month).should be_true
+#
+#    day = Regexp.build( 1..31 )
+#    ("6"    =~ day).should be_true
+#    ("16"   =~ day).should be_true
+#    ("Tues" =~ day).should be_false
+#
+#    year = Regexp.build( 98, 99, 2000..2005 )
+#    ("04"   =~ year).should be_false
+#    ("2004" =~ year).should be_true
+#    ("99"   =~ year).should be_true
+#
+#    num = Regexp.build( 0..1_000_000 )
+#    ("-1"   =~ num).should be_false
+#  end
 end
+
+def sanitize(s)
+  s.to_s.gsub(/[()\\]/, '').gsub(/[?][^:]+[:]/, '')
+end
+
+#describe "Range" do
+#  it "should convert to a regex" do
+#    sanitize((13..33).to_regexp).should == '[1][3-9]|[2][0-9]|[3][0-3]'
+#  end
+#end
+#
+#describe "Fixnum" do
+#  it "should round down to the nearest multiple of ten" do
+#    [ 11, 25, 33, 40 ].map(&:round).should == [ 10, 20, 30, 40 ]
+#  end
+#end
